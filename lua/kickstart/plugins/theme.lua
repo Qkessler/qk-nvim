15a030aec6c100bcc4bffbd0c99556f6d9825cdf
Enrique Kessler Martínez <enrikes@amazon.com>
2024-05-18 19:48:06 +0200

    Use gruvbox-material as the theme

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -4,12 +4,18 @@ return {
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    'RRethy/base16-nvim',
+    'sainnhe/gruvbox-material',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
+      vim.g.gruvbox_material_better_performance = 1
+      -- Themes
+      vim.g.gruvbox_material_foreground = 'mix'
+      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_ui_contrast = 'high' -- The contrast of line numbers, indent lines, etc.
+
+      vim.cmd.colorscheme 'gruvbox-material'
       vim.o.background = 'dark'
     end,
   },
